24c13075305fba968a699faca451a2ad7ed95e14
unknown <ranjatinasoa@gmail.com>
2025-10-02 23:27:27 +0300

    maj pas encore fini

--- components/oi.py
+++ components/oi.py
@@ -1,41 +1,31 @@
-from kivy.lang import Builder
-from kivymd.uix.menu import MDDropdownMenu
-
 from kivymd.app import MDApp
-
-from models.gestionModel import GestionModel
+from kivy.lang import Builder
 
 KV = '''
-MDScreen
-    md_bg_color: self.theme_cls.backgroundColor
+MDScreen:
 
-    MDDropDownItem:
-        pos_hint: {"center_x": .5, "center_y": .5}
-        on_release: app.open_menu(self)
+    MDTopAppBar:
+        title: "Palette CSS test"
+        pos_hint: {"top": 1}
+        elevation: 4
 
-        MDDropDownItemText:
-            id: drop_text
-            text: "selectionnez"
+    MDLabel:
+        text: "Bonjour !"
+        halign: "center"
+        pos_hint: {"center_y": 0.6}
+        theme_text_color: "Primary"
+
+    MDButton:
+        text: "Clic"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        md_bg_color: "#f48935"  # ton orange
+        text_color: "white"
 '''
 
-
-class Example(MDApp):
-    def open_menu(self, item):
-        instance = GestionModel()
-        product_types = instance.get_type
-        menu_items = [
-            {
-                "text": f"{i}",
-                "on_release": lambda x=f"{i}": self.menu_callback(x),
-            } for i in product_types
-        ]
-        MDDropdownMenu(caller=item, items=menu_items).open()
-
-    def menu_callback(self, text_item):
-        self.root.ids.drop_text.text = text_item
-
+class MyApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = "Light"  # ou "Dark"
+        self.theme_cls.primary_palette = "Darkslateblue"  # compatible CSS name
         return Builder.load_string(KV)
 
-
-Example().run()
+MyApp().run()
